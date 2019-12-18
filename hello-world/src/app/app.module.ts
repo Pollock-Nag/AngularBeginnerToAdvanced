878ff9b18d9889b6b7eb18008d97577e356ba7cc
Jorge Eliecer <jorge.m.h@outlook.com>
2019-12-18 08:39:04 -0500

    components and interpolation

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesComponentComponent } from './components/courses-component/courses-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ hello-world/src/app/components/courses-component/courses-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-component',
+  templateUrl: './courses-component.component.html',
+  styleUrls: ['./courses-component.component.css']
+})
+export class CoursesComponentComponent implements OnInit {
+
+  title = 'List of Courses';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
