1f75b9a73d617a8a6543413742f4247518fa0acd
Jorge Eliecer <jorge.m.h@outlook.com>
2019-12-17 23:14:58 -0500

    like component

---
+++ hello-ts/modules.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Modules
+var point_1 = require("./point");
+// Object    constructor parameters
+var point = new point_1.Point(1, 2);
+point.draw();
