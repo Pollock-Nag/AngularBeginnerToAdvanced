73ff6cc476d879903047612d7751a67635b542ac
Jorge Eliecer <jorge.m.h@outlook.com>
2019-12-24 12:09:25 -0500

    ngFor directive more complete

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -8,6 +8,41 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'directives';
   courses = [1, 2, 3];
+
   // NgSwitchCase
   viewMode = 'map';
+
+  // NgFor
+  coursesFor = [
+    { id: 1, name: 'Course 1'},
+    { id: 2, name: 'Course 2'},
+    { id: 3, name: 'Course 3'},
+    { id: 4, name: 'Course 4'},
+  ];
+
+  coursesLoad;
+
+  onAdd() {
+    this.coursesFor.push({id: 5, name: 'Course 5'});
+  }
+
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.coursesFor.splice(index, 1);
+  }
+  onChange(course) {
+    course.name = 'UPDATED'
+  }
+
+  loadCoursesMethod() {
+    this.coursesLoad = [
+      { id: 1, name: 'Course load 1'},
+      { id: 2, name: 'Course load 2'},
+      { id: 3, name: 'Course load 3'}
+    ]
+  }
+
+  trackCourse(index, courseLoad) {
+    return courseLoad ? courseLoad.id : undefined;
+  }
 }
