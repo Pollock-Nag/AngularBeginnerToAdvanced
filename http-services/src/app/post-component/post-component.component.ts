a76b35c9001f21759e7fe1771c5a60549db49160
Jorge Eliecer <jorge.m.h@outlook.com>
2019-12-26 17:11:10 -0500

    first HTTP service connection

--- http-services/src/app/post-component/post-component.component.ts
+++ http-services/src/app/post-component/post-component.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-post-component',
   templateUrl: './post-component.component.html',
   styleUrls: ['./post-component.component.css']
 })
-export class PostComponentComponent implements OnInit {
+export class PostComponentComponent {
+  posts: any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient) {
+    http.get(this.url)
+    .subscribe(response => {
+      this.posts = response as any;
+    });
   }
 
+  createPost(input: HTMLInputElement) {
+    const post = { title: input.value };
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        response = response as Object;
+        post['id'] = response.id;
+        this.posts.splice(0, 0, post);
+        console.log(response);
+      });
+  }
 }
