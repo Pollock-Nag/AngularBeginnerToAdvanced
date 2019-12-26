78ddd54233ff74840f791b5e68d22a552eee68cd
Jorge Eliecer <jorge.m.h@outlook.com>
2019-12-26 10:53:34 -0500

    implementing custom validations

---
+++ reactive-forms/src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsernameValidations } from './username.validator';
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css']
+})
+export class SignupFormComponent implements OnInit {
+
+  form = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      UsernameValidations.cannotContainSpace
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ])
+  });
+
+  // Properties
+  get username() {
+    return this.form.get('username');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
