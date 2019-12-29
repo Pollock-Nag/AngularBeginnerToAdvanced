34a088e8018d32be1a486cd9b06fdb9bbace0c0f
Jorge Eliecer <jorge.m.h@outlook.com>
2019-12-28 22:53:49 -0500

    rebuild components and app.modulwe

--- navigation/src/app/app.module.ts
+++ navigation/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { GithubFollowersComponent } from './components/github-followers/github-followers.component';
+import { GithubProfileComponent } from './components/github-profile/github-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
-    BrowserModule
+
+  BrowserModule,
+    RouterModule.forRoot([
+      { path: '/', component: HomeComponent },
+      { path: 'followers', component: GithubFollowersComponent },
+      { path: 'profile/:username', component: GithubProfileComponent }
+    ]),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
