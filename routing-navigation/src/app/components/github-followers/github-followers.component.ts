b0127a3c2b6202f87dfbb4d91f9e529cc1af3122
Jorge Eliecer <jorge.m.h@outlook.com>
2019-12-30 15:39:10 -0500

    switchMapOperator modification on data.service

--- routing-navigation/src/app/components/github-followers/github-followers.component.ts
+++ routing-navigation/src/app/components/github-followers/github-followers.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubFollowersService } from '../../services/github-followers.service';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest } from 'rxjs';
+import { map, filter, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-github-followers',
@@ -8,13 +11,25 @@ import { GithubFollowersService } from '../../services/github-followers.service'
 })
 export class GithubFollowersComponent implements OnInit {
 
-  followers: any[];
+  followers: any;
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService) { }
 
   ngOnInit(): void {
-    this.service.getAll()
-      .subscribe(followers => this.followers = followers as any);
-  }
 
+    combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ])
+    .pipe(
+      switchMap(combined => {
+      const id = combined[0].get('id');
+      const page = combined[1].get('page');
+
+      return this.service.getAll();
+    }))
+      .subscribe(followers => this.followers = followers);
+  }
 }
