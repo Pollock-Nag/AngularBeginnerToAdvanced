f77d6ed3e736e2b656b90181fac66fbfd9a9982b
Jorge Eliecer <jorge.m.h@outlook.com>
2019-12-30 17:09:20 -0500

    last components created and app.module routes

--- auth-app/src/app/app.module.ts
+++ auth-app/src/app/app.module.ts
@@ -1,20 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NoAccessComponent } from './no-access/no-access.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SignupComponent } from './signup/signup.component';
+
+import { OrderService } from './services/order.service';
+import { AuthService } from './services/auth.service';
+import { fakeBackendProvider } from './helpers/fake-backend';
+import { MockBackend } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    NoAccessComponent,
+    NotFoundComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'admin', component: AdminComponent },
+      { path: 'login', component: LoginComponent },
+      { path: '', component: NoAccessComponent },
+      { path: '**', component: NotFoundComponent}
+    ])
   ],
-  providers: [],
+  providers: [
+    OrderService,
+    AuthService,
+
+    // Fake Backend
+    fakeBackendProvider,
+    MockBackend,
+    BaseRequestOptions
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
