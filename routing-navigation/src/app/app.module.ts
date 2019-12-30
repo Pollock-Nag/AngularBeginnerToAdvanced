a3441ed90920e7be15ec6b86da71a5b2a6fdab04
Jorge Eliecer <jorge.m.h@outlook.com>
2019-12-30 12:31:17 -0500

    change component to a new on Angular V6.0

---
+++ routing-navigation/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { FollowersComponent } from './components/followers/followers.component';
+import { GithubFollowersComponent } from './components/github-followers/github-followers.component';
+import { GithubProfileComponent } from './components/github-profile/github-profile.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PostsComponent } from './components/posts/posts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FollowersComponent,
+    GithubFollowersComponent,
+    GithubProfileComponent,
+    HomeComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'followers/:id', component: GithubProfileComponent },
+      { path: 'followers', component: GithubFollowersComponent },
+      { path: 'posts', component: PostsComponent },
+      { path: '**', component: NotFoundComponent }
+    ]),
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
