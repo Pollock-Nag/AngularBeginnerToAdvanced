6f775521c42a5761f7c4c860f74616e9f82ded35
Jorge Eliecer <jorge.m.h@outlook.com>
2019-12-31 17:11:54 -0500

    order.service last change

--- auth-app/src/app/services/order.service.ts
+++ auth-app/src/app/services/order.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { AuthHttp } from 'angular2-jwt';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor(private authHttp: AuthHttp) { }
+  constructor(private authHttp: AuthHttp, private http: HttpClient) { }
 
   getOrders() {
     return this.authHttp.get('/api/orders')
