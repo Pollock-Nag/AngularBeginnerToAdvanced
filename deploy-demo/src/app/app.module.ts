409ad14664b29e1cb1b1ca7f5b6f453405716d2a
Jorge Eliecer <jorge.m.h@outlook.com>
2020-01-02 10:27:38 -0500

    Angular compiler in action AOT

--- deploy-demo/src/app/app.module.ts
+++ deploy-demo/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router'; 
-import { HttpModule } from "@angular/http";
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { GithubFollowersComponent } from './github-followers/github-followers.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
 
 import { GithubFollowersService } from './github-followers/github-followers.service';
 
@@ -16,15 +17,17 @@ import { GithubFollowersService } from './github-followers/github-followers.serv
     AppComponent,
     HomeComponent,
     GithubFollowersComponent,
+    GithubProfileComponent,
     AboutComponent,
     NavbarComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
-      { path: 'followers', component: GithubFollowersComponent }
+      { path: 'followers', component: GithubFollowersComponent },
+      { path: 'followers/:id/:username', component: GithubProfileComponent },
     ])
   ],
   providers: [
--- deploy-demo/src/app/github-followers/github-followers.service.ts
+++ deploy-demo/src/app/github-followers/github-followers.service.ts
@@ -1,15 +1,16 @@
-import { Http } from '@angular/http';
+
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class GithubFollowersService {
-  private readonly _url = 'https://api.github.com/users/mosh-hamedani/followers';
+  private readonly _url = 'https://api.github.com/users/jorgeemherrera/followers';
 
-  constructor(private http: Http) {  }
+  constructor(private http: HttpClient) {  }
 
-  getFollowers() { 
+  getFollowers() {
     return this.http.get(this._url)
-      .map(response => response.json());
+      .map(response => response);
   }
 }
