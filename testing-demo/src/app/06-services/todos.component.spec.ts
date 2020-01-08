ca7957f4693c97ff4e899f6ba08681a60a61ed49
Jorge Eliecer <jorge.m.h@outlook.com>
2020-01-08 11:57:47 -0500

    unit testing  section done

--- testing-demo/src/app/06-services/todos.component.spec.ts
+++ testing-demo/src/app/06-services/todos.component.spec.ts
@@ -1,7 +1,7 @@
-import { TodosComponent } from './todos.component';
+import { of, Observable, throwError, empty } from 'rxjs';
 import { TodoService } from './todo.service';
-import { Observable, of } from 'rxjs';
-import { from } from 'rxjs';
+import { TodosComponent } from './todos.component';
+import { startWith } from 'rxjs/operators';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -20,6 +20,48 @@ describe('TodosComponent', () => {
 
     // expect(component.todos.length).toBeGreaterThan(0);
     expect(todos.length).toBe(3);
+  });
 
+  it('should call the server to save the changes when a new todo item is added', () => {
+    const spy = spyOn(service, 'add').and.callFake(t => {
+      // return Observable.empty();
+      return of();
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+  it('should add the new todo returned from the server', () => {
+    const todo = { id: 1 };
+    const spy = spyOn(service, 'add').and.returnValue(of([todo]));
+
+    component.add();
+
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+  });
+  it('should set the message property if server returns an error when adding a new todo', () => {
+    const error = 'error from the server';
+    const spy = spyOn(service, 'add').and.returnValue(throwError(error));
+
+    component.add();
+
+    expect(component.message).toBe(error);
+  });
+  it('should call teh server to delete a todo item if the user confirms', () => {
+    spyOn(window, 'confirm').and.returnValue(true);
+    const spy = spyOn(service, 'delete').and.returnValue(empty());
+
+    component.delete(1);
+
+    expect(spy).toHaveBeenCalledWith(1);
+  });
+  it('should NOT call teh server to delete a todo item if the user cancels', () => {
+    spyOn(window, 'confirm').and.returnValue(false);
+    const spy = spyOn(service, 'delete').and.returnValue(empty());
+
+    component.delete(1);
+
+    expect(spy).not.toHaveBeenCalled();
   });
 });
